fa2df2992446bc12234c9ea1d1fba48e49c9b320
kaio cristian costa silva <kaiodeckmaster@hotmail.com>
2012-09-26 14:41:28 -0300

    Capítulo 14
    Completo.

--- Projeto-Financeiro_Generico/src/financeiro/web/UsuarioBean.java
+++ Projeto-Financeiro_Generico/src/financeiro/web/UsuarioBean.java
@@ -1,15 +1,18 @@
 package financeiro.web;
 
+import java.util.List;
+import java.util.Set;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-import java.util.List;
-import java.util.*;
 
+import financeiro.conta.Conta;
+import financeiro.conta.ContaRN;
 import financeiro.usuario.Usuario;
 import financeiro.usuario.UsuarioRN;
-import financeiro.conta.*;
+import financeiro.web.util.RNException;
 
 @ManagedBean(name = "usuarioBean")
 @RequestScoped
@@ -19,15 +22,74 @@ public class UsuarioBean {
 	private String confirmarSenha;
 	private List<Usuario> lista;
 	private String destinoSalvar;
+	private String permissao;
 	private Conta conta = new Conta();
 
 	public String novo() {
-		
 		this.destinoSalvar = "usuarioSucesso";
 		this.usuario = new Usuario();
 		this.usuario.setAtivo(true);
 		return "usuario";
-		
+	}
+
+	public String editar() {
+		this.confirmarSenha = this.usuario.getSenha();
+		return "/publico/usuario";
+	}
+
+	public String salvar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		String senha = this.usuario.getSenha();
+		if (!senha.equals(this.confirmarSenha)) {
+			FacesMessage facesMessage = new FacesMessage(
+					"A senha não foi confirmada corretamente");
+			context.addMessage(null, facesMessage);
+			return null;
+		}
+
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+
+		if (this.conta.getDescricao() != null) {
+			this.conta.setUsuario(this.usuario);
+			this.conta.setFavorita(true);
+			ContaRN contaRN = new ContaRN();
+			contaRN.salvar(this.conta);
+		}
+
+		// Envia email após o cadastramento de um usuário novo
+		if (this.destinoSalvar.equals("usuarioSucesso")) {
+			try {
+				usuarioRN.enviarEmailPosCadastramento(this.usuario);
+			} catch (RNException e) {
+				FacesMessage facesMessage = new FacesMessage(
+						"Não foi possível enviar o e-mail de cadastro do usuário. Erro: "
+								+ e.getMessage());
+				context.addMessage(null, facesMessage);
+				return null;
+			}
+		}
+
+		return this.destinoSalvar;
+	}
+
+	public String excluir() {
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.excluir(this.usuario);
+		this.lista = null;
+		return null;
+	}
+
+	public String ativar() {
+		if (this.usuario.isAtivo())
+			this.usuario.setAtivo(false);
+		else
+			this.usuario.setAtivo(true);
+
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		return null;
 	}
 
 	public String atribuiPermissao(Usuario usuario, String permissao) {
@@ -37,135 +99,58 @@ public class UsuarioBean {
 		Set<String> permissoes = this.usuario.getPermissao();
 
 		if (permissoes.contains(permissao)) {
-			
 			permissoes.remove(permissao);
-			
 		} else {
-			
 			permissoes.add(permissao);
-			
 		}
-
 		return null;
-	}
-
-	public String editar() {
-
-		this.confirmarSenha = this.usuario.getSenha();
-		return "/publico/usuario";
-
-	}
-
-	public String excluir() {
-
-		UsuarioRN usuarioRN = new UsuarioRN();
-		usuarioRN.excluir(this.usuario);
-		this.lista = null;
-		return null;
-		
-	}
-
-	public String salvar() {
-		
-		FacesContext context = FacesContext.getCurrentInstance();
-		
-		String senha = this.usuario.getSenha();
-		
-		if (!senha.equals(this.confirmarSenha)) {
-			
-			FacesMessage facesMessage = new FacesMessage(
-					"A senha não foi confirmada corretamente");
-			context.addMessage(null, facesMessage);
-			return null;
-
-		}
-		
-		UsuarioRN usuarioRN = new UsuarioRN();
-		usuarioRN.salvar(this.usuario);
-
-		if (this.conta.getDescricao() != null) {
-
-			this.conta.setUsuario(this.usuario);
-			this.conta.setFavorita(true);
-			ContaRN contaRN = new ContaRN();
-			contaRN.salvar(this.conta);
-			
-		}
-
-		return this.destinoSalvar;
-
-	}
-
-	public Conta getConta() {
-		
-		return conta;
-		
-	}
-
-	public void setConta(Conta conta) {
-		
-		this.conta = conta;
-		
-	}
-
-	public String ativar() {
-		
-		if (this.usuario.isAtivo())
-			this.usuario.setAtivo(false);
-		else
-			this.usuario.setAtivo(true);
-
-		UsuarioRN usuarioRN = new UsuarioRN();
-		usuarioRN.salvar(this.usuario);
-		return null;
-		
 	}
 
 	public List<Usuario> getLista() {
-
 		if (this.lista == null) {
 			UsuarioRN usuarioRN = new UsuarioRN();
 			this.lista = usuarioRN.listar();
 		}
-
 		return this.lista;
-
-	}
-
-	public String getDestinoSalvar() {
-		
-		return destinoSalvar;
-		
-	}
-
-	public void setDestinoSalvar(String destinoSalvar) {
-		
-		this.destinoSalvar = destinoSalvar;
-		
 	}
 
 	public Usuario getUsuario() {
-		
-		return usuario;
-		
+		return this.usuario;
 	}
 
 	public void setUsuario(Usuario usuario) {
-		
 		this.usuario = usuario;
-		
 	}
 
 	public String getConfirmarSenha() {
-		
 		return confirmarSenha;
-		
 	}
 
 	public void setConfirmarSenha(String confirmarSenha) {
-		
 		this.confirmarSenha = confirmarSenha;
-		
 	}
 
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
+	}
+
+	public String getPermissao() {
+		return permissao;
+	}
+
+	public void setPermissao(String permissao) {
+		this.permissao = permissao;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
 }
--- Projeto-Financeiro_Generico/src/financeiro/web/util/MensagemUtil.java
+++ Projeto-Financeiro_Generico/src/financeiro/web/util/MensagemUtil.java
@@ -1,6 +1,7 @@
 package financeiro.web.util;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javax.faces.context.FacesContext;
@@ -21,6 +22,23 @@ public class MensagemUtil {
 	public static String getMensagem(String propriedade, Object... parametros) {
 
 		String mensagem = getMensagem(propriedade);
+		MessageFormat formatter = new MessageFormat(mensagem);
+		return formatter.format(parametros);
+
+	}
+
+	public static String getMensagem(Locale locale, String propriedade) {
+
+		ResourceBundle bundle = ResourceBundle.getBundle(
+				MensagemUtil.PACOTE_MENSAGENS_IDIOMAS, locale);
+		return bundle.getString(propriedade);
+
+	}
+
+	public static String getMensagem(Locale locale, String propriedade,
+			Object... parametros) {
+
+		String mensagem = getMensagem(locale, propriedade);
 		MessageFormat formatter = new MessageFormat(mensagem);
 		return formatter.format(parametros);
 
